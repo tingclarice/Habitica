333b1f6d3cea12f4beafb59a206b403262736595
Clarice <clariceting2006@gmail.com>
2025-05-13 12:52:50 +0700

    Added a simple menu interface

---
+++ Habitica/src/Logic/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    // STARTING SCREEN
+
+    public void start() {
+        System.out.println("""
+                ========================================
+                            🌱 HABITICA 🌱
+                    Health Habit Tracker Application
+                ========================================
+
+                Welcome to Habitica!
+
+                [1] Sign Up
+                [2] Log In
+                [3] Exit
+
+                Stay healthy by keeping track of your habits! 💪
+                """);
+
+        System.out.print("Option: ");
+
+        // Switch case
+    }
+
+    // SIGN UP & LOG IN
+    public void signUp() {}
+
+    public void logIn() {}
+
+    public void exit() {}
+
+    // USER INTERFACE
+    public void menu() {
+        System.out.println("""
+                ========================================
+                            🌱 HABITICA 🌱
+                    Health Habit Tracker Application
+                ========================================
+
+                [1] Add Habit
+                [2] View Habit Log
+                [3] Edit Habit
+                [4] Delete Habit
+                [5] Log Out
+
+                """);
+        
+        System.out.print("Option: ");
+        // Switch case
+    }
+
+    // HABIT MANAGEMENT
+    public void addHabitMenu() {}
+
+    public void viewHabitLog() {}
+
+    public void editHabit() {}
+
+    public void deleteHabit() {}
+
+    
+
+}
